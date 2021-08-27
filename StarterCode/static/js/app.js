c3e5a3e58a8ebbc8d82430f84ec6d7d37fe372ee
Jackson Freese <79999864+jfreese816@users.noreply.github.com>
2021-08-27 14:23:52 -0700

    Add files via upload

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,38 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log( tableData);
+
+var tbody = d3.select( "tbody");
+
+data.forEach( function( sighting) {
+    console.log( sighting);
+    var row = tbody.append( "tr");
+    Object.entries( sighting).forEach( function( [key, value]) {
+        console.log( key, value);
+        var cell = tbody.append( "td");
+        cell.text( value);
+    });
+});
+
+// Search button click event
+var button = d3.select( "#filter-btn");
+
+button.on( "click", function( event){
+    d3.event.preventDefault();
+    tbody.html( "");
+
+// Define filters
+var inputDate = d3.select( "#datetime"); 
+var dateValue = inputDate.property( "value");
+
+// Filter search results
+var filter = tableData.filter( tableData => tableData.datetime === dateValue);
+filter.forEach( function( dateSelect){
+    var row = tbody.append( "tr");
+    Object.entries( dateSelect).forEach( function( [key, value]) {
+    var cell = tbody.append( "td");
+    cell.text( value);
+        });
+    });
+});
